eabfbb787929bed0631529b6e41d78d1601f00d1
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2018-07-11 20:41:09 +0300

    Add project files.

---
+++ RuntimeAppLanguage/LanguageManager.cs
@@ -0,0 +1,67 @@
+﻿using Android.Content;
+using Android.OS;
+
+using Java.Util;
+
+namespace RuntimeAppLanguage
+{
+    internal class LanguageManager
+    {
+        private const string MYLANGUAGE = "myLanguage";
+        private const string MYPREF = "myPreference";
+
+        public static Context LoadLanguage(Context context)
+        {
+            var loadedLanguage = GetLanguage(context, Locale.Default.Language);
+            return ChangeLanguage(context, loadedLanguage);
+        }
+
+        public static Context ChangeLanguage(Context context, string language)
+        {
+            SaveLanguage(context, language);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.N)
+            {
+                return ChangeForAPI24(context, language);
+            }
+            return ChangeForLegacy(context, language);
+        }
+
+        private static string GetLanguage(Context context, string Language)
+        {
+            var privatePreference = context.GetSharedPreferences(MYPREF, FileCreationMode.Private);
+            return privatePreference.GetString(MYLANGUAGE, Language);
+        }
+
+        private static void SaveLanguage(Context context, string language)
+        {
+            var privatePreference = context.GetSharedPreferences(MYPREF, FileCreationMode.Private);
+            var editor = privatePreference.Edit();
+            editor.PutString(MYLANGUAGE, language);
+            editor.Apply();
+        }
+
+        private static Context ChangeForAPI24(Context context, string language)
+        {
+            // for api >= 24
+            var locale = new Locale(language);
+            Locale.Default = locale;
+            var configuration = context.Resources.Configuration;
+            configuration.SetLocale(locale);
+            configuration.SetLayoutDirection(locale);
+
+            return context.CreateConfigurationContext(configuration);
+        }
+
+        private static Context ChangeForLegacy(Context context, string language)
+        {
+            var locale = new Locale(language);
+            Locale.Default = locale;
+            var resources = context.Resources;
+            var configuration = resources.Configuration;
+            configuration.Locale = locale;
+            resources.UpdateConfiguration(configuration, resources.DisplayMetrics);
+
+            return context;
+        }
+    }
+}
---
+++ RuntimeAppLanguage/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace RuntimeAppLanguage
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        V7Toolbar mainToolbar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            mainToolbar = FindViewById<V7Toolbar>(Resource.Id.mainToolbar);
+
+            //Toolbar will now take on default actionbar characteristics
+            SetSupportActionBar(mainToolbar);
+
+            SupportActionBar.Title = GetString(Resource.String.app_name);
+        }
+
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(LanguageManager.LoadLanguage(@base));
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.language_menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.arItem:
+                    LanguageManager.ChangeLanguage(this, "ar");
+                    Recreate();
+                    return true;
+                case Resource.Id.enItem:
+                    LanguageManager.ChangeLanguage(this, "en");
+                    Recreate();
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+    }
+
+
+}
+
